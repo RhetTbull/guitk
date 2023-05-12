8a74d20c128b7c0b6ee832d2c851ba1526f66195
Rhet Turnbull <rturnbull@gmail.com>
2023-05-12 07:35:53 -0700

    Added add_widget to VStack, HStack

---
+++ examples/add_widgets_at_runtime.py
@@ -0,0 +1,39 @@
+"""Demo showing how to add widgets at runtime"""
+
+import tkinter as tk
+
+from guitk import (
+    Button,
+    HLayout,
+    HStack,
+    Label,
+    VSeparator,
+    VStack,
+    Window,
+    on,
+)
+
+
+class Demo(Window):
+    def config(self):
+        with HLayout():
+            with VStack() as self.vs:
+                Button("Say Hello", key="Hello")
+                Label("Hello")
+            VSeparator()
+            with HStack() as self.hs:
+                Button("Say Goodbye", key="Goodbye")
+
+    @on(key="Hello")
+    def on_hello(self):
+        label = Label("Hello")
+        self.vs.add_widget(label)
+
+    @on(key="Goodbye")
+    def on_goodbye(self):
+        label = Label("Goodbye")
+        self.hs.add_widget(label)
+
+
+if __name__ == "__main__":
+    Demo().run()
