275365028ed8ec8d3dcfc8f8dfbb6f4b97170455
Rhet Turnbull <rturnbull@gmail.com>
2023-05-30 06:14:54 -0700

    Added Treeview to demo

--- guitk/__main__.py
+++ guitk/__main__.py
@@ -1,34 +1,42 @@
 """Demo of GUITk widgets and layouts"""
 
+import pathlib
 import sys
 import tkinter as tk
 
-from guitk import (
-    BrowseDirectoryButton,
-    BrowseFileButton,
-    Button,
-    Checkbutton,
-    CheckButton,
-    DebugWindow,
-    Entry,
-    Event,
-    EventType,
-    HLayout,
-    HSeparator,
-    HStack,
-    Label,
-    LabelEntry,
-    LinkLabel,
-    Notebook,
-    Output,
-    Tab,
-    VerticalTab,
-    VLayout,
-    VSeparator,
-    VStack,
-    Window,
-    on,
-)
+from guitk import *
+
+dummy_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempus neque in vehicula hendrerit. Nam non posuere ante. Nunc libero libero, tempus eget enim vitae, egestas hendrerit tortor. Vivamus et egestas felis. Aliquam erat volutpat. Nulla facilisi. Aliquam hendrerit, nibh nec tempor lobortis, purus nisl vehicula ex, dapibus fermentum mauris nibh id nulla. Vivamus non pretium quam. Phasellus elementum commodo nisl. Nullam eu faucibus augue. Vivamus pulvinar metus vehicula urna porttitor euismod. "
+
+
+def get_docstring(name):
+    """Return the docstring of an object with name"""
+    try:
+        obj = globals()[name]
+    except KeyError as e:
+        raise ValueError(f"Invalid object name: {e}") from e
+    return obj.__doc__ or ""
+
+
+def list_files(path: str, tree: Treeview):
+    """list files in a directory and add to Treeview tree"""
+    files = pathlib.Path(path).iterdir()
+    pyfiles = []
+    for f in files:
+        if f.suffix == ".py":
+            pyfiles.append(str(f))
+            tags = ["pyfile"]
+        else:
+            tags = []
+        tree.widget.insert(
+            "",
+            "end",
+            iid=str(f),
+            tags=tags,
+            text=str(f),
+            values=(str(f), f.stat().st_size),
+        )
+    tree.widget.selection_set(pyfiles)
 
 
 class Demo(Window):
@@ -49,7 +57,7 @@ class Demo(Window):
                         LabelEntry("Enter some text:", key="entry", events=True)
                         Label("You entered:")
                         Label("", key="label_entered")
-                    with HStack():
+                    with HStack(valign="center"):
                         LinkLabel("This Label is a link", key="link").font(
                             underline=True
                         ).style(foreground="blue")
@@ -59,6 +67,63 @@ class Demo(Window):
                         CheckButton(
                             "Other checkbutton", key="check_other", disabled=True
                         )
+                        Button("Start Timer", key="start_timer")
+                        Label("0", key="timer", width=4)
+                        ComboBox(
+                            default="PNG",
+                            values=["PNG", "JPEG", "GIF"],
+                            width=4,
+                            readonly=True,
+                        )
+                    with HStack():
+                        with LabelFrame("Radio Buttons"):
+                            with VStack():
+                                Label("These RadioButtons are in a LabelFrame")
+                                with HStack():
+                                    with VStack(expand=False):
+                                        RadioButton(
+                                            "Option A", key="option_a", group="group1"
+                                        )
+                                        RadioButton(
+                                            "Option B", key="option_b", group="group1"
+                                        )
+                                    with VStack(expand=False):
+                                        RadioButton(
+                                            "Option 1", key="option_1", group="group2"
+                                        )
+                                        RadioButton(
+                                            "Option 2", key="option_2", group="group2"
+                                        )
+                        with VStack():
+                            Label("These Text boxes are in an HStack")
+                            with HStack():
+                                with VStack():
+                                    Label("Text Box", pady=0)
+                                    Text(
+                                        key="text_box_1",
+                                        text=dummy_text,
+                                        height=4,
+                                        tooltip="Text box",
+                                        pady=0,
+                                    )
+                                with VStack(padding=0):
+                                    Label("Text Box With Scrollbar", pady=0)
+                                    Text(
+                                        key="text_box_2",
+                                        text=dummy_text,
+                                        height=4,
+                                        vscrollbar=True,
+                                        tooltip="Text box with scrollbar",
+                                        pady=0,
+                                    )
+                    with HStack():
+                        Treeview(
+                            key="treeview",
+                            headings=["Filename", "Size"],
+                            show="headings",
+                            tooltip="Treeview; click on headings to sort",
+                            vscrollbar=True,
+                        ),
                 with Tab("Tab 2"):
                     ...
             with HStack():
@@ -71,10 +136,35 @@ class Demo(Window):
                     Button("stderr")
 
     def setup(self):
-        ...
-        # self["output"].enable_redirect()
+        """gets called right after __init__"""
+        print(f"Setting up: {self.__dict__}")
+
+        # check the Enable checkbutton for enabled output redirect
+        self.get("check_enable").widget.invoke()
+
+        # set up the treeview
+        self.tree_sort_size_reverse = False
+        self.tree_sort_filename_reverse = False
+        tree = self.get("treeview")
+        list_files(".", self.get("treeview"))
+
+        # bind events for treeview
+        tree.bind_heading("Size", "tree_heading_size")
+        tree.bind_heading("Filename", "tree_heading_filename")
+
+        # handle Return and double-click events for treeview, but only for python files
+        # list_files() will tag python files with tag 'pyfile'
+        tree.bind_tag("pyfile", "tree_python_file", sequence="<Return>")
+        tree.bind_tag("pyfile", "tree_python_file", sequence="<Double-1>")
+
+        print("Done with setup")
+
+    def teardown(self):
+        """gets called right before window is destroyed"""
+        print("Tearing down")
 
     def handle_event(self, event: Event):
+        """Gets called for every event"""
         print(event)
 
     @on(key="debug_window")
@@ -88,8 +178,10 @@ class Demo(Window):
     @on(key="check_enable")
     def on_check_enable(self):
         if self["check_enable"].value:
+            print("Enabling output redirect")
             self["output"].enable_redirect()
         else:
+            print("Disabling output redirect")
             self["output"].disable_redirect()
 
     @on(key="check_echo")
@@ -109,8 +201,57 @@ class Demo(Window):
         print("You clicked the link")
 
     @on(key="check_enable_other")
-    def on_check_enable(self):
+    def on_check_enable_other(self):
         self["check_other"].disabled = not self["check_enable_other"].value
+
+    @on(key="start_timer")
+    def on_start_timer(self):
+        """Start/stop the timer"""
+        # cancel the timer if it is running
+        if self.timer_id:
+            self.cancel_timer_event(self.timer_id)
+            self.timer_id = None
+            self.get("start_timer").value = "Start Timer"
+            return
+
+        # start the timer
+        self.timer_id = self.bind_timer_event(1000, "<<MyTimer>>", repeat=True)
+        self.get("start_timer").value = "Stop Timer"
+
+    @on(key="<<MyTimer>>")
+    def on_timer(self):
+        """Increment the timer"""
+        self["timer"].value = int(self["timer"].value) + 1
+
+    @on(key="tree_heading_size")
+    def on_tree_heading_size(self):
+        """Sort treeview on size"""
+        self.get("treeview").sort_on_column(
+            "Size",
+            reverse=self.tree_sort_size_reverse,
+            key=lambda x: int(x[0]),
+        )
+        self.tree_sort_size_reverse = not self.tree_sort_size_reverse
+
+    @on(key="tree_heading_filename")
+    def on_tree_heading_filename(self):
+        """Sort treeview on filename"""
+        self.get("treeview").sort_on_column(
+            "Filename",
+            reverse=self.tree_sort_filename_reverse,
+            key=lambda x: x[0],
+        )
+        self.tree_sort_filename_reverse = not self.tree_sort_filename_reverse
+
+    @on(key="tree_python_file")
+    def on_tree_python_file(self):
+        """User hit return on a Python file"""
+        print("You picked a Python file")
+
+    @on(key="treeview")
+    def on_treeview(self):
+        """User clicked on a treeview item"""
+        print(f"You clicked on a treeview item: {self.get('treeview').value}")
 
 
 if __name__ == "__main__":
