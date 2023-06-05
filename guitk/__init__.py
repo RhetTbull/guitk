497b5f6e695a16f84a9195db6ff7684a1767776e
Rhet Turnbull <rturnbull@gmail.com>
2023-06-05 06:43:22 -0700

    Version bump

--- guitk/__init__.py
+++ guitk/__init__.py
@@ -49,7 +49,7 @@ from .ttk_treeview import Listbox, ListBox, Treeview, TreeView
 from .widget import Widget, widget_class_factory
 from .window import Window
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Rhet Turnbull"
 
 __all__ = [
